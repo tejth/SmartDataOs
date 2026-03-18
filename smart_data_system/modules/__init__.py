8ea16ef509266309faf66f44ff22bd3796bdbde1
Tejendra Pal Singh <110801292+tejth@users.noreply.github.com>
2026-03-18 05:40:47 +0530

    Delete smart_data_system directory

--- smart_data_system/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from .validation import UserFormValidator
-from .processing import DataProcessor
-from .serialization import save_result, load_all_results, delete_result
-from .threading_tasks import run_threaded_processing
--- smart_data_system/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .decorators import log_call, timer, validate_input
-from .iterators import DatasetRowIterator, RangeStepIterator
-from .generators import chunk_dataset, stats_stream, json_record_generator
-from .mixins import SerializableMixin, LoggableMixin, ReprMixin
